f5b05dff9270e9488d4958503f6d4b965429649e
qasimali97 <qasim.ali@netsoltech.com>
2019-11-26 19:13:12 +0500

    new update

--- Controllers/menuSellingCtrl.js
+++ Controllers/menuSellingCtrl.js
@@ -1,3 +1,49 @@
 app.controller("menuSellingCtrl", function($scope) {
+
+
+  // =================
+  
+  var tabs = [
+    { title: 'One', content: "Tabs will become paginated if there isn't enough room for them."}],
     
+  selected = null,
+  previous = null;
+$scope.tabs = tabs;
+$scope.selectedIndex = 0;
+$scope.$watch('selectedIndex', function(newVal, oldVal) {
+  previous = selected;
+  selected = tabs[newVal];
+  if (oldVal + 1 && !angular.equals(oldVal, newVal)) {
+    $log.log('Goodbye ' + previous.title + '!');
+  }
+  if (newVal + 1 > 0) {
+    $log.log('Hello ' + selected.title + '!');
+  }
+});
+
+// add tabs
+$scope.addTab = function(title) {
+  tabs.push({title: title});
+};
+
+// remove tabs 
+$scope.removeTab = function(tab) {
+  var index = tabs.indexOf(tab);
+  tabs.splice(index, 1);
+};
+
+  // Sticky Header Script
+  window.onscroll = function() {
+    myFunction();
+  };
+  var header = document.getElementById("myHeader");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
 });
--- Controllers/workqueueDashboardCtrl.js
+++ Controllers/workqueueDashboardCtrl.js
@@ -2,16 +2,18 @@ app.controller("workqueueDashboardCtrl", function($scope) {
   $scope.searchObj = {};
   $scope.searchObj.days = 1;
 
-
-
-  // Map Api 
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1IjoicWFzaW1hbGk5NzciLCJhIjoiY2szOG10dzdvMDhicDNibDNtbmFteHA5biJ9.7pJAWdLewua6Pi8q_bMyhA}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+  // Map Api
+  var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+  L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1IjoicWFzaW1hbGk5NzciLCJhIjoiY2szOG10dzdvMDhicDNibDNtbmFteHA5biJ9.7pJAWdLewua6Pi8q_bMyhA}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: "your.mapbox.access.token"
+    }
+  ).addTo(mymap);
 
   // Sticky Header Script
   window.onscroll = function() {
